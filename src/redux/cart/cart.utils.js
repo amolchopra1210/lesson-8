640b99144cdeb4d74a4f50c4021f45be998aeb40
Amol <amolviper@gmail.com>
2020-07-19 22:49:36 +0530

    completed checkout compomnentt

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -6,3 +6,12 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     return [...cartItems, {...cartItemToAdd, quantity: 1}];
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id); //check if item already exists in cart items arry
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    return cartItems.map(cartItem => cartItem.id === cartItemToRemove.id ? {...cartItem, quantity: cartItem.quantity - 1}: cartItem)
+}
